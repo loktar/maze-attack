2b219f4b2c729081f57036123813cf89fd3d06b8
Ian Fisher <loktar@gmail.com>
2019-02-16 13:58:27 -0800

    Camera positions itself inside the player's head

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPosition = player.transform.position;
+        float playerHeight = player.transform.localScale.y;
+        float halfPlayerHeight = playerHeight / 2f;
+
+        this.transform.position = playerPosition + new Vector3(0, halfPlayerHeight, 0);
+    }
+}
