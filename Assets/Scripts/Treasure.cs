99605aece884ad697f80129d157c7283871b91f3
Ian Fisher <loktar@gmail.com>
2019-02-17 19:11:28 -0800

    Added treasure that you can collect

---
+++ Assets/Scripts/Treasure.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Treasure : MonoBehaviour
+{
+    public TreasureTracker treasureTracker;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<Player>())
+        {
+            treasureTracker.FindTreasure(this);
+        }
+    }
+}
---
+++ Assets/Scripts/TreasureTracker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TreasureTracker : MonoBehaviour
+{
+    public int treasuresFound = 0;
+    public Text hudText;
+
+    private void Start()
+    {
+        UpdateHUDText();
+    }
+
+    public void FindTreasure(Treasure treasure)
+    {
+        treasuresFound++;
+        Destroy(treasure.gameObject);
+        UpdateHUDText();
+    }
+
+    private void UpdateHUDText()
+    {
+        hudText.text = "treasures found: " + treasuresFound;
+    }
+}
